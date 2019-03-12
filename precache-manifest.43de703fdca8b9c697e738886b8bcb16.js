2ff586382650d6962b4ca82c943f9dc38c9e6ca2
Wejdi <wejdigharbi@yahoo.fr>
2019-03-12 22:46:55 +0100

    Updates

---
+++ precache-manifest.43de703fdca8b9c697e738886b8bcb16.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6979a4ea6ecf44c5fe08045261b48278",
+    "url": "/Movie-App-React-Redux/static/media/Film4.6979a4ea.jpg"
+  },
+  {
+    "revision": "4282b18e4086430d72c9",
+    "url": "/Movie-App-React-Redux/static/css/main.8bedcd4b.chunk.css"
+  },
+  {
+    "revision": "f64ab595e5ff4cde621d",
+    "url": "/Movie-App-React-Redux/static/js/runtime~main.f64ab595.js"
+  },
+  {
+    "revision": "2194e531900726ba2a0f",
+    "url": "/Movie-App-React-Redux/static/js/2.2194e531.chunk.js"
+  },
+  {
+    "revision": "0c96cb97e6f67f31325adb4623cc980c",
+    "url": "/Movie-App-React-Redux/static/media/Film1.0c96cb97.jpg"
+  },
+  {
+    "revision": "129f604413b75357537a3b7fef2937e4",
+    "url": "/Movie-App-React-Redux/static/media/Film3.129f6044.jpg"
+  },
+  {
+    "revision": "4282b18e4086430d72c9",
+    "url": "/Movie-App-React-Redux/static/js/main.4282b18e.chunk.js"
+  },
+  {
+    "revision": "de2fb77eed1dc3e7eb0700135d022fac",
+    "url": "/Movie-App-React-Redux/static/media/Film5.de2fb77e.jpg"
+  },
+  {
+    "revision": "8915a463798eea03a3d7272ab19718ba",
+    "url": "/Movie-App-React-Redux/static/media/Film6.8915a463.jpg"
+  },
+  {
+    "revision": "04baca55182c24265d6bcdebedc4c516",
+    "url": "/Movie-App-React-Redux/static/media/Film7.04baca55.jpg"
+  },
+  {
+    "revision": "fe227a5276345a8305747e118a4682f3",
+    "url": "/Movie-App-React-Redux/static/media/Film8.fe227a52.jpg"
+  },
+  {
+    "revision": "97d4292c80cfaf85fdd84df5708c3d85",
+    "url": "/Movie-App-React-Redux/static/media/bg-title-shop.97d4292c.png"
+  },
+  {
+    "revision": "5eb42d6b477d0616ff9c9947621a6db6",
+    "url": "/Movie-App-React-Redux/index.html"
+  }
+];
